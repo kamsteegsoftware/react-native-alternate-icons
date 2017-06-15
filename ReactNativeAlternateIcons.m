122be1d1296efb87dcaf0075467b879a1be25c0d
Joey Kamsteeg <joey_kamsteeg@live.nl>
2017-06-15 11:57:54 +0200

    iOS version check

--- ReactNativeAlternateIcons.m
+++ ReactNativeAlternateIcons.m
@@ -5,50 +5,68 @@
 
 @implementation ReactNativeAlternateIcons
 
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
 RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(setIconName:(NSString *)name){
-    NSLog(@"Setting the following icon: %@", name);
-    [[UIApplication sharedApplication] setAlternateIconName:name completionHandler:^(NSError * _Nullable error) {
-        if( error != nil ){
-            NSLog(@"Error: %@", error.description );
-        }
-    }];
+    if( SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"10.3") ){
+        [[UIApplication sharedApplication] setAlternateIconName:name completionHandler:^(NSError * _Nullable error) {
+            if( error != nil ){
+                NSLog(@"Error: %@", error.description );
+            }
+        }];
+    }
 }
 
 RCT_EXPORT_METHOD(reset){
-    [[UIApplication sharedApplication] setAlternateIconName:nil completionHandler:^(NSError * _Nullable error) {
-        if( error != nil ){
-            NSLog(@"Error: %@", error.description );
-        }
-    }];
+    if( SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"10.3") ){
+        [[UIApplication sharedApplication] setAlternateIconName:nil completionHandler:^(NSError * _Nullable error) {
+            if( error != nil ){
+                NSLog(@"Error: %@", error.description );
+            }
+        }];
+    }
 }
 
 RCT_EXPORT_METHOD(getIconName:(RCTResponseSenderBlock) callback ){
-    //resolve( [[UIApplication sharedApplication] alternateIconName] );]
-    NSString *name = [[UIApplication sharedApplication] alternateIconName];
-    if( name == nil ){
-        name = @"default";
+    NSString *name = @"default";
+    NSDictionary *results;
+    
+    if( SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"10.3") ){
+        if( [[UIApplication sharedApplication] supportsAlternateIcons ] ){
+            name = [[UIApplication sharedApplication] alternateIconName];
+            if( name == nil ){
+                name = @"default";
+            }
+        }
     }
     
-    NSDictionary *results = @{
-                              @"iconName":name
-                              };
-    
+    results = @{
+                @"iconName":name
+                };
     callback(@[results]);
 }
 
 RCT_EXPORT_METHOD(supportDevice:(RCTResponseSenderBlock) callback){
-    NSDictionary *results;
+    NSDictionary *results = @{
+                              @"supported":@NO
+                              };
     
-    if( [[UIApplication sharedApplication] supportsAlternateIcons ] ){
-        results = @{
-            @"supported":YES
-        };
-    }else{
-        results = @{
-            @"supported":NO
-        };
+    if( SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"10.3") ){
+        if( [[UIApplication sharedApplication] supportsAlternateIcons ] ){
+            results = @{
+                        @"supported":@YES
+                        };
+        }else{
+            results = @{
+                        @"supported":@NO
+                        };
+        }
     }
     
     callback(@[results]);
